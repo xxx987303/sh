a88a909fea79f354bd5b1085efc3dffb1d608d9a
xxx987303 <xxx987303@gmail.com>
2025-11-05 18:53:16 +0100

    .

--- installer.sh
+++ installer.sh
@@ -10,10 +10,10 @@ echo "Copying /assets/"
 for dir in 1 5980 5907; do
     rsync -avc$dryRun --delete --exclude "*~" --exclude "#*#" \
 	  $GIT/site/assets/files/$dir/ \
-	  $SIT/site/assets/files/$dir/ | grep -v -E "^(Transfer|$|sent|total)|/$" 
+	  $SIT/site/assets/files/$dir/ | grep -v -E "^(Transfer|$|sent|total)|/$" | sed "s/^/  /"
 done
 
 echo "Copying /site/"
 rsync -avc$dryRun --delete --exclude "*~" --exclude "#*#" --exclude assets \
       $GIT/site/ \
-      $SIT/site/ | grep -v -E "^(Transfer|$|sent|total)|/$"
+      $SIT/site/ | grep -v -E "^(Transfer|$|sent|total)|/$" | sed "s/^/  /"
