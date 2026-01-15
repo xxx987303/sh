087f1f038c1d61dd57fe31b959f720e23eccdbe8
xxx987303 <103523432+xxx987303@users.noreply.github.com>
2026-01-15 19:21:12 +0100

    Installer fixes

--- tools/i_git2site.sh
+++ tools/i_git2site.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
+#
+# iMac2013 runs an old mysql, old php, etc, be carefull...
+#
 
 [ $(hostname -s) == "lap2022-M2" ] && { echo "??? You can't run it from the \"Master host\""; exit; } 
 
-dryRun=n
+function say(){
+    echo; echo "=========================================================" $@
+}
+
+dryRun=
 [ -n "$dryRun" ] && echo "...DRY RUN"
 
+[ $(hostname -s) = "iMac2013" ] && SQL="cms-pw-sh_iMac2013.sql" || SQL="cms-pw-sh.sql"
+say "SQL file is \"$SQL\""
+DB=cms-pw-sh
 GIT=~/github/sh.git
 SIT=~/Sites/sh
 
-echo "Copying /assets/"
+say "Copying /assets/"
 rsync -avc$dryRun --delete --exclude "*~" --exclude "#*#" \
       $GIT/site/assets/files/ \
       $SIT/site/assets/files/ | grep -v -E "^(Transfer|$|sent|total)|/$" | sed "s/^/  /"
 
-echo "Copying /site/"
+say "Copying /site/"
 rsync -avc$dryRun --delete --exclude "*~" --exclude "#*#" --exclude assets \
       $GIT/site/ \
       $SIT/site/ | grep -v -E "^(Transfer|$|sent|total)|/$" | sed "s/^/  /"
+
+say "Refreshing database"
+set -x
+mysql -u yb -p $DB < $GIT/$SQL
